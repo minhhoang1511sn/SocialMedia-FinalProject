4e655b79e166891581b08472dd77b5c64384a00f
minhhoang1511sn <80461876+minhhoang1511sn@users.noreply.github.com>
2023-10-22 16:27:00 +0700

    update login phone

--- src/main/java/com/social/socialnetwork/dto/PhoneLoginRequest.java
+++ src/main/java/com/social/socialnetwork/dto/PhoneLoginRequest.java
@@ -4,13 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class PhoneLoginRequest {
-
-        private String password;
         private String phone;
-
+        private String password;
 }
