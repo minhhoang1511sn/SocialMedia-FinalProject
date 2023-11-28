aff791e681b69e39e126f3427d88f97c48fd7eac
minhhoang1511sn <80461876+minhhoang1511sn@users.noreply.github.com>
2023-11-28 22:42:52 +0700

    sa

--- src/main/java/com/social/socialnetwork/model/Video.java
+++ src/main/java/com/social/socialnetwork/model/Video.java
@@ -1,11 +1,7 @@
 package com.social.socialnetwork.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
