1437129742ac5d0b9ae76ac04d4fdd0de145da36
minhhoang1511sn <80461876+minhhoang1511sn@users.noreply.github.com>
2024-01-01 22:42:56 +0700

    cross

--- src/main/java/com/social/socialnetwork/Controller/admin/AdminController.java
+++ src/main/java/com/social/socialnetwork/Controller/admin/AdminController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/admin/")
 @RequiredArgsConstructor
+@CrossOrigin(origins ="http://localhost:3000")
 public class AdminController {
     private final UserService userService;
     private final ReportService reportService;
